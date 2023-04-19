b553fce6646b9d4b0c1d428a57991a5b1b81401a
BartlomiejSkoczylas7991 <BartlomiejSkoczylas7991@gmail.com>
2023-04-19 12:23:00 +0200

    pagination, card etc

--- client/src/app/components/product-list/product-list.component.ts
+++ client/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,9 @@
+import { SearchComponent } from './../search/search.component';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,8 +15,21 @@ export class ProductListComponent implements OnInit{
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  currentCategoryName: string = "";
+  searchMode: boolean = false;
+
+  // for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElemensts: number = 0;
+
+  previousKeyword: string = "";
+
+
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute){ }
 
   ngOnInit(): void {
@@ -23,24 +39,91 @@ export class ProductListComponent implements OnInit{
     }
 
   listProducts(){
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    // check if "id" parameter is available
+    if (this.searchMode){
+      this.handleSearchProducts();
+    }
+    else{
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // if we have a different keyword than previous then set thePageNumber to 1
+
+    if (this.previousKeyword != theKeyword){
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
+    // now search for the products using keyword
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                                this.thePageSize,
+                                                theKeyword).subscribe(this.processResult());
+  }
+
+  handleListProducts() {
+     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId){
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
     else{
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
+    // if there is a different category id than previous then thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`)
+
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                              this.thePageSize,
+                                              this.currentCategoryId)
+                                              .subscribe(this.processResult());
+  }
+
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+  processResult(){
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElemensts = data.page.totalElements;
+    };
+  }
+
+  addToCart(theProduct: Product){
+
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
   }
 }
