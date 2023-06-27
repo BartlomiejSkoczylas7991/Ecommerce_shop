dc477b95932b67b59691f555c6d00221627f66f0
BartlomiejSkoczylas7991 <BartlomiejSkoczylas7991@gmail.com>
2023-06-27 13:23:50 +0200

    27

---
+++ README.txt
@@ -0,0 +1,50 @@
+# Nazwa aplikacji
+
+## Wstęp
+
+(Krótki opis aplikacji, jej cel i funkcje)
+
+## Technologie
+
+- Spring Boot
+- Angular
+- MySQL
+- Docker
+
+## Wymagania
+
+Podaj wymagania do uruchomienia aplikacji, np.:
+
+- JDK 11
+- Node.js v18
+- Docker
+- Docker Compose
+
+## Instrukcje instalacji i konfiguracji
+
+(Tutaj powinieneś opisać, jak skonfigurować środowisko do uruchomienia Twojej aplikacji)
+
+1. Klonuj repozytorium: `git clone https://github.com/twoje-repo.git`
+2. Zainstaluj zależności: (Tutaj powinny znaleźć się instrukcje dotyczące instalacji zależności)
+3. Ustaw zmienną środowiskową: `export...` (Jeżeli to jest konieczne)
+4. Uruchom aplikację: `docker-compose up` (lub inna komenda do uruchomienia)
+
+## Jak używać aplikacji
+
+(Podaj tutaj krótkie instrukcje na temat tego, jak korzystać z Twojej aplikacji)
+
+## Testy
+
+(Jeżeli masz testy, podaj instrukcje jak je uruchomić)
+
+## API
+
+(Jeżeli masz API, podaj tutaj jego specyfikacje)
+
+## Autorzy
+
+(Twoje imię i nazwisko, a także dane kontaktowe)
+
+## Licencja
+
+(Jeżeli masz licencję, podaj tutaj jej szczegóły)
--- client/src/app/components/checkout/checkout.component.ts
+++ client/src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 import { CheckoutService } from 'src/app/services/checkout.service';
 import { Order } from 'src/app/common/order';
 import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 
 @Component({
   selector: 'app-checkout',
@@ -190,19 +191,63 @@ export class CheckoutComponent implements OnInit{
     }
     */
 
-    let orderItemsShort: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
 
 
     // set up purchase
     let purchase = new Purchase();
 
+    //populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
     // populate purchase - shipping address
-    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress']
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingCountry: State = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
     // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingCountry: State = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
 
     // populate purchase - order and orderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
 
     // call REST API via the CheckoutService
+    this.checkoutService.placeOrder(purchase).subscribe(
+      {
+        next: response => {
+          alert('Your order has been received. \n Order tracking number: ${response.orderTrackingNumber}');
+
+          // reset cart
+          this.resetCart();
+
+        },
+        error: err => {
+          alert('There was an error: ${err.message}');
+        }
+      }
+    )
+
+  }
+
+  resetCart() {
+    // reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    // reset the form
+    this.checkoutFormGroup.reset();
+
+    // navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
 
   handleMonthsAndYears() {
