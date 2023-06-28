4f795fc694562c4791f3d371e9ef64402eede9a8
BartlomiejSkoczylas7991 <BartlomiejSkoczylas7991@gmail.com>
2023-06-28 15:05:25 +0200

    próba dockeryzacji i uwierzytelnianie

---
+++ client/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+  oidc: {
+    clientId: 'HVbDyc9cOu2VziYUuAY98BseUpyK2n4n',
+    issuer: 'https://dev-dk7ytdvopi3p52qh.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
+
+
